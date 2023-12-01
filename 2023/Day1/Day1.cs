949467cbfe0cc019976bcf5dedf5fe02dd61f274
WillyDev3344 <108835059+WillyDev3344@users.noreply.github.com>
2023-12-01 14:13:11 -0500

    Day 1 part 1

---
+++ 2023/Day1/Day1.cs
@@ -0,0 +1,28 @@
+﻿using AdventOfCode.Utils;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode._2023.Day1
+{
+    class Day1 : Solution
+    {
+        protected override string ExecutePart1(List<string> inputContent)
+        {
+            int sum = 0;
+            foreach (string line in inputContent)
+            {
+                string allNumbers = Regex.Replace(line, @"[^\d]", "");
+                char[] number = [allNumbers[0], allNumbers[^1]];
+                int num = int.Parse(new string(number));
+
+                sum += num;
+            }
+            
+            return sum.ToString();
+        }
+
+        protected override string ExecutePart2(List<string> inputContent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿using AdventOfCode._2023.Day1;
+
+Day1 day1 = new Day1();
+day1.Solve();
