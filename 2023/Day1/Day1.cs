12987c432493231307b35c764201aad6a9d01ccf
WillyDev3344 <108835059+WillyDev3344@users.noreply.github.com>
2023-12-02 10:52:05 -0500

    Day 1 P2 Solve

--- 2023/Day1/Day1.cs
+++ 2023/Day1/Day1.cs
@@ -22,7 +22,43 @@ namespace AdventOfCode._2023.Day1
 
         protected override string ExecutePart2(List<string> inputContent)
         {
-            throw new NotImplementedException();
+            Dictionary<string, int> numberConversionDict = new()
+            {
+                { "one", 1},
+                {"two", 2 },
+                {"three", 3 },
+                {"four", 4 },
+                {"five", 5 },
+                {"six", 6 },
+                {"seven", 7 },
+                {"eight", 8 },
+                {"nine", 9 }
+            };
+
+            string pattern = @"(?=(" + string.Join("|", numberConversionDict.Keys.Select(Regex.Escape)) + @"|\d+))";
+
+            int sum = 0;
+
+            foreach (string line in inputContent)
+            {
+                MatchCollection matches = Regex.Matches(line, pattern);
+
+                string firstMatch = matches.First().Groups[1].Value;
+                string lastMatch = matches.Last().Groups[1].Value;
+
+                int firstNum = 10;
+                int lastNum = 1;
+
+                if (!char.IsDigit(firstMatch[0])) firstNum *= numberConversionDict[firstMatch];
+                else firstNum *= (int)char.GetNumericValue(firstMatch[0]);
+
+                if (!char.IsDigit(lastMatch[0])) lastNum *= numberConversionDict[lastMatch];
+                else lastNum *= (int)char.GetNumericValue(lastMatch[0]);
+
+                sum += (firstNum + lastNum);
+            }
+
+            return sum.ToString();
         }
     }
 }
